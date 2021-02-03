4b1752ae47374c374b3732340b50b33ca92a1b5f
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-03 16:29:24 +0500

    Location error messages entered

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ const App = () => {
 	const [longitude, setLongitude] = useState(260.1)
 	const [res, setRes] = useState()
 	const [show, setShow] = useState(false)
+	const [loadingErrorCode, setLoadingErrorCode] = useState(0)
 
 	const showNav = () => setShow(!show)
 
 	const location = () => {
 		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(position => {
-				setLatitude(position.coords.latitude)
-				setLongitude(position.coords.longitude)
-			})
+			navigator.geolocation.getCurrentPosition(
+				position => {
+					setLatitude(position.coords.latitude)
+					setLongitude(position.coords.longitude)
+				},
+				error => {
+					setLoadingErrorCode(error.code)
+					console.log(error)
+				}
+			)
 		} else {
 			alert('Geolocation is not supported by this browser.')
 		}
 	}
 
-	const getWeatherData = () => {
+	const getWeatherDataByLatitudeLongitude = () => {
 		axios
 			.get(
 				`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=272621386d2b0b9953a5efa44597d57e&units=metric`
@@ -34,7 +41,7 @@ const App = () => {
 	useEffect(() => {
 		location(setLatitude, setLongitude)
 		if (longitude !== 260.1 && latitude !== 260.1) {
-			getWeatherData(latitude, longitude)
+			getWeatherDataByLatitudeLongitude(latitude, longitude)
 		}
 	}, [longitude, latitude])
 
@@ -42,7 +49,28 @@ const App = () => {
 		if (res) {
 			return <AppUI data={res} />
 		} else {
-			return <h1>Loading, Wait..</h1>
+			return (
+				<div>
+					<h1 style={{ marginTop: '25vh' }}>
+						{loadingErrorCode === 0
+							? 'Loading, Please wait..'
+							: loadingErrorCode === 1
+							? 'User denied the request for location.'
+							: loadingErrorCode === 2
+							? 'Location information is unavailable.'
+							: 'Unknown Error, try refreashing'}
+					</h1>
+					<h2 style={{ marginTop: '10%' }}>
+						{loadingErrorCode === 0
+							? ''
+							: loadingErrorCode === 1
+							? 'Please Allow location. then refresh page.'
+							: loadingErrorCode === 2
+							? 'Your location is unavailable. Try search your city from side bar'
+							: 'Try search your city from side bar'}
+					</h2>
+				</div>
+			)
 		}
 	}
 
