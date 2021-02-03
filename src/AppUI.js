2119f7a2ed0c38fcfc3d23ff2fed877ce296f509
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-03 12:11:45 +0500

    Side navbar added for weather searching

--- src/AppUI.js
+++ src/AppUI.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AppUI = () => {
+	const [show, setShow] = useState(false)
+
+	const showNav = () => setShow(!show)
+
 	return (
 		<div className='app'>
+			<button onClick={showNav} className='nav-btn'>
+				&#9776;
+			</button>
+			<div
+				className='side-nav'
+				style={{ transform: `scaleX(${show ? 1 : 0})` }}>
+				<h3>
+					Search weather by city name, state name, and country name
+				</h3>
+				<input type='text' name='cityName' placeholder='City Name...' />
+				<input
+					type='text'
+					name='stateName'
+					placeholder='State Name...'
+				/>
+				<input
+					type='text'
+					name='countryName'
+					placeholder='Country Name...'
+				/>
+				<button className='btn'>Show Weather</button>
+			</div>
 			<h1>Hyderabad Weather</h1>
 			<div className='main'>
 				<div className='main-one'>
