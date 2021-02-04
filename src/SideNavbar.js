ed63b7f0092f8ed295946f80d6fe8802d53e06e1
Rehan Ali <rehan.shah70@yahoo.com>
2021-02-04 16:32:10 +0500

    now side bar will provide city and country data

--- src/SideNavbar.js
+++ src/SideNavbar.js
@@ -1,19 +1,31 @@
 import React from 'react'
 
-const SideNavbar = ({ show }) => {
+const SideNavbar = ({
+	show,
+	getWeatherData,
+	setCity,
+	setCountry
+}) => {
 	return (
 		<div
 			className='side-nav'
 			style={{ transform: `scaleX(${show ? 1 : 0})` }}>
 			<h3>Search weather by city name, state name, and country name</h3>
-			<input type='text' name='cityName' placeholder='City Name...' />
-			<input type='text' name='stateName' placeholder='State Name...' />
+			<input
+				type='text'
+				name='cityName'
+				placeholder='City Name...'
+				onChange={e => setCity(e.target.value.toLowerCase())}
+			/>
 			<input
 				type='text'
 				name='countryName'
 				placeholder='Country Name...'
+				onChange={e => setCountry(e.target.value.toLowerCase())}
 			/>
-			<button className='btn'>Show Weather</button>
+			<button onClick={getWeatherData} className='btn'>
+				Show Weather
+			</button>
 		</div>
 	)
 }
